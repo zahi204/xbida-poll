2468c61fd7d45cfc9af07530d4bd97271ea4c77f
zahi <zissa6236@gmail.com>
2020-08-31 16:34:35 +0300

    view changes

--- resources/views/pages/dashboard-analytics.blade.php
+++ resources/views/pages/dashboard-analytics.blade.php
@@ -43,108 +43,139 @@
 <center>
 
 
+<img class="round" src="{{asset('images/portrait/small/logomet.png') }}" alt="avatar" height="100" width="250" />
 
 
-<div class="card">
+<!--*
+<div class="card" style="height:200px; width:800px; border-width: 5px; border-color: #706f6f; border-style: solid; border-radius: 5%;">
           <div class="card-content  show" aria-expanded="true">
-              <div class="card-body" style="padding:2rem">
-              <h1 style="font-size:5rem; text-align: right;";><b>איך הגעת אלינו ؟؟</b></h1>
+              <div class="card-body" style="padding:1rem">
+              <h1 style="font-size:4rem; text-align: right;";><u>עזור לנו להתמקד </u></h1>
 
-    <br> 
-    <br> 
+   
 
 
-    <h2 style="font-size:4rem;text-align: right;";>תודה שקנית איצלנו , חושב לנו לדעת איפה ראית אותנו ؟؟</h2>
+    <h2 style="font-size:3rem;text-align: center;";>תודה שקנית אצלנו <br> חשוב לנו לדעת איפה ראית אותנו ؟؟</h2>
     
         </div>
           </div>
       </div>
    
     </center>
+-->
+<br>
+<img class="" src="{{asset('images/portrait/small/titleblack.png') }}" height="230" width="850" />
+<br>
+<br>
+<br>
 
 
 
-  <div class="row">
-   
+<div class="container" style="width:800px; "; >  
 
+
+
+
+
+
+<div class="row" style="";>
   
-<div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
-      <div class="card">
-          <div class="card-content  show" aria-expanded="true">
-              <div class="card-body" style="padding:0rem">
-                <center>
-              <button id="fbBtn" onClick="switchColor('fbBtn','fbBtn2')" style="height:100%;width:100%;  background-color:white; font-size:2.5rem;font-family: 'Cairo', sans-serif; "; class="btn  btn-inline"><span><img class="round" src="{{asset('images/portrait/small/fb.png') }}" alt="avatar" height="150" width="150" /></span></span><br><br> Facebook</button>
-              </center>
-        </div>
+  
+  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3" style="margin-left:0px;";>
+        <div class="card" style="border-width: 0px; border-color: #706f6f; border-style: solid;  border-radius: 10%; height:250px; width:150px;";>
+            <div class="card-content  show" aria-expanded="true">
+                <div class="card-body" style="padding:0rem">
+                  <center>
+                <button id="fbBtn" onClick="switchColor('fbBtn','fbBtn2')" style="border-width: 0px; border-color: #706f6f; border-style: solid;  border-radius: 10%; height:250px; width:150px;  background-color:white; font-size:2.5rem;font-family: 'Cairo', sans-serif; "; class="btn  btn-inline"><span><img style="margin-top:30px;"; class="round" src="{{asset('images/portrait/small/fb.png') }}" alt="avatar" height="75" width="75" /><br><br><img style="margin-left:-10px;"; class="round" src="{{asset('images/portrait/small/fbtext2.png') }}" alt="avatar" height="50" width="120" /></span></span><br><br></button>
+                </center>
           </div>
-      </div>
-</div>
-
-
-
-
-
-
-<div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
-      <div class="card">
-          <div class="card-content collapse show" aria-expanded="true">
-              <div class="card-body" style="padding:0rem">
-                <center>
-              <button id="TikBtn" onClick="switchColor('TikBtn','TikBtn2')" style="height:100%;width:100%;  background-color:white; font-size:2.5rem;font-family: 'Cairo', sans-serif; "; class="btn  btn-inline"><span><img class="round" src="{{asset('images/portrait/small/tik.png') }}" alt="avatar" height="150" width="150" /></span></span><br><br>TikTok</button>
-              </center>
+            </div>
         </div>
-          </div>
-      </div>
-</div>
-
-
-
-
-
-<div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
-      <div class="card">
-          <div class="card-content collapse show" aria-expanded="true">
-              <div class="card-body" style="padding:0rem">
-                <center>
-              <button  id="IgBtn" onClick="switchColor('IgBtn','IgBtn2')" style="height:100%;width:100%;  background-color:white; font-size:2.5rem;font-family: 'Cairo', sans-serif; "; class="btn  btn-inline"><span><img class="round" src="{{asset('images/portrait/small/inst.png') }}" alt="avatar" height="150" width="150" /></span></span><br><br>Instagram</button>
-              </center>
-        </div>
-          </div>
-      </div>
-</div>
-
-
-
-
-
-
-
-
-  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
-      <div class="card" style="";>
-          <div class="card-content collapse show" aria-expanded="true">
-              <div class="card-body" style="padding:0rem">
-                <center>
-              <button  id="OthBtn" onClick="switchColor('OthBtn','OthBtn2')" style="height:100%;width:100%;  background-color:white; font-size:2.5rem;font-family: 'Cairo', sans-serif; "; class="btn  btn-inline"><span><img class="round" src="{{asset('images/portrait/small/oth2.png') }}" alt="avatar" height="150" width="150" /></span></span><br><br><b>אחר</b></button>
-              </center>
-        </div>
-          </div>
-      </div>
-</div>
-
-
-
-
-
-
-
-
-
-
   </div>
+  
+  
+  
+  
+  
+  
+  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3" style="margin-left:0px;";>
+        <div class="card" style="border-width: 0px;border-color: #706f6f;border-style: solid;  border-radius: 10%; height:250px; width:150px;";>
+            <div class="card-content collapse show" aria-expanded="true">
+                <div class="card-body" style="padding:0rem">
+                  <center>
+                <button id="TikBtn" onClick="switchColor('TikBtn','TikBtn2')" style="border-width: 0px; border-color: #706f6f; border-style: solid;  border-radius: 10%; height:250px; width:150px;  background-color:white; font-size:2.5rem;font-family: 'Cairo', sans-serif; "; class="btn  btn-inline"><span><img style="margin-top:30px;"; class="round" src="{{asset('images/portrait/small/tik.png') }}" alt="avatar" height="75" width="75" /><br><br><img style="margin-left:-15px;"; class="" src="{{asset('images/portrait/small/tiktext2.png') }}" alt="avatar" height="50" width="120" /></span></span><br><br></button>
+                </center>
+          </div>
+            </div>
+        </div>
+  </div>
+  
+  
+  
+  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3" style="margin-left:0px;";>
+        <div class="card" style="border-width: 0px; border-color: #706f6f; border-style: solid;  border-radius: 10%; height:250px; width:150px;";>
+            <div class="card-content collapse show" aria-expanded="true">
+                <div class="card-body" style="padding:0rem">
+                  <center>
+                <button  id="IgBtn" onClick="switchColor('IgBtn','IgBtn2')" style="border-width: 0px; border-color: #706f6f; border-style: solid;  border-radius: 10%; height:250px; width:150px;  background-color:white; font-size:2.5rem;font-family: 'Cairo', sans-serif; "; class="btn  btn-inline"><span><img class="round" src="{{asset('images/portrait/small/inst.png') }}" alt="avatar" height="75" width="75" /><br><br><img style="margin-left:-10px;"; class="round" src="{{asset('images/portrait/small/insttext.png') }}" alt="avatar" height="50" width="120" /></span></span></button>
+                </center>
+          </div>
+            </div>
+        </div>
+  </div>
+  
+  
+  
+  
+  
+  
+  
+  
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3" style="margin-left:0px;";>
+        <div class="card" style="border-width: 0px; border-color: #706f6f; border-style: solid;  border-radius: 10%; height:250px; width:150px;";>
+            <div class="card-content collapse show" aria-expanded="true">
+                <div class="card-body" style="padding:0rem">
+                  <center>
+                <button  id="OthBtn" onClick="switchColor('OthBtn','OthBtn2')" style="border-width: 0px; border-color: #706f6f; border-style: solid;  border-radius: 10%; height:250px; width:150px;  background-color:white; font-size:2.5rem;font-family: 'Cairo', sans-serif; "; class="btn  btn-inline"><span><img style="margin-left:-15px;margin-top:25px;"; class="" src="{{asset('images/portrait/small/oth.png') }}" alt="avatar" height="130" width="130" /></span></span><br><br><b></b></button>
+                </center>
+          </div>
+            </div>
+        </div>
+  </div>
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    </div>
+  
 
 
-    
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+  <center>
+<!-- Button trigger modal -->
+<button  style="visibility: hidden;"; id="subbtn" onClick="submitForm()" type="button" class="   btn "  data-toggle="modal" data-target="#exampleModal" >
+<img class="" src="{{asset('images/portrait/small/sendbtn.png') }}" alt="avatar" height="70" width="135" />
+</button>
+</center>
 
 
 <form id='myForm' name="resultForm" action="{{ route('saveData') }}" method="post">
@@ -156,31 +187,16 @@
 <input Hidden name="company_id" type="text" class="form-control" id=""  value="<?php echo $company_id; ?>" placeholder="" >
 <input Hidden name="branch_id" type="text" class="form-control" id=""  value='<?php echo $branch_id; ?>' placeholder="" >
 
-<center>
-<!-- Button trigger modal -->
-<button onClick="submitForm()" type="button" class="btn btn-primary" style="margin:20px;height:75px; width:180px; font-size:30px; font-family: 'Cairo', sans-serif;" data-toggle="modal" data-target="#exampleModal">
-שלח
-</button>
-</center>
+
 
 
 
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div style=" margin-top:150px;"; class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg" role="document">
-    <div class="modal-content">
-
-      <div class="modal-body">
-      <center>
-      <img class="round" src="{{asset('images/portrait/small/logo.png') }}" alt="avatar" height="200" width="300" />
-
-<img class="round" src="{{asset('images/portrait/small/thks.png') }}" alt="avatar" height="300" width="550" />
-
-    </center>      
-  </div>
+  <img class="round" src="{{asset('images/portrait/small/thanksblack.png') }}" alt="avatar" height="300" width="800" />
     
-    </div>
   </div>
 </div>
 
@@ -191,45 +207,66 @@
 
   <script>
 
+(function () {
+ 
+
+
+ 
+
+
+}());
+
+
+
+
+
+
 
 function switchColor(btnColor,btnRes){
         
-        var btnC = document.getElementById(btnColor);
-        var btnR = document.getElementById(btnRes);
-        var value=btnR.value;
+      var btnC = document.getElementById(btnColor);
+      var btnR = document.getElementById(btnRes);
+      var value=btnR.value;
 
       var btnFace = document.getElementById('fbBtn');
       var btnInsta = document.getElementById('IgBtn');
       var btnTik = document.getElementById('TikBtn');
 
-      var btnFaceVal = document.getElementById('fbBtn2');
-      var btnInstaVal = document.getElementById('IgBtn2');
-      var btnTikVal = document.getElementById('TikBtn2');
+
       
-
-
         if(value==0){
-
-        btnC.style.background = '#bcc9fe';
+        btnC.style.background = '#ffd966';
         btnR.value='1';
-
-          if(btnColor=='OthBtn'){
-            btnFace.style.background = 'white';
-            btnFaceVal.value='0';
-
-            btnInsta.style.background = 'white';
-            btnInstaVal.value='0';
-
-            btnTik.style.background = 'white';
-            btnTikVal.value='0';
-
-          }
+        document.getElementById('subbtn').style.visibility = "visible";
 
 
         }else{
         btnC.style.background = 'white';
         btnR.value='0'; 
+
+        var btnFaceVal = parseInt(document.getElementById('fbBtn2').value);
+      var btnInstaVal = parseInt(document.getElementById('IgBtn2').value);
+      var btnTikVal = parseInt(document.getElementById('TikBtn2').value);
+      var btnOthVal = parseInt(document.getElementById('OthBtn2').value);
+
+
+
+      if(btnFaceVal==1 || btnInstaVal==1 || btnTikVal==1 || btnOthVal==1){
+        document.getElementById('subbtn').style.visibility = "visible";
+
+          }else{
+            document.getElementById('subbtn').style.visibility = "hidden";
+
+
+          }
+
+
+      
+
         }
+
+    
+
 
 }
 
